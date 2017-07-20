9370117454fa9f5dd55a9336c1d925cc13d7a2e3
Szász Tibor <tszasz@gmail.com>
2017-07-20 16:13:20 +0200

    feat(autocomplete): created messy poc

--- src/override/app.js
+++ src/override/app.js
@@ -1,7 +1,9 @@
 const suggestions = document.querySelector('ul');
 const input = document.querySelector('input');
 const suggestApi = document.querySelector('#suggest');
+const directions = ['ArrowUp','ArrowDown','ArrowRight','ArrowLeft'];
 let apiAccess = null;
+let sIndex = 0;
 
 document.body.addEventListener('click', e => {
     e.preventDefault();
@@ -30,12 +32,28 @@ input.addEventListener('keyup', e => {
         let url = 'https://www.google.hu/search?site=&source=hp&q=' + input.value;
         window.location.href = url;
     }
+
+    if( e.key == 'ArrowDown' ) {
+        highlightSuggestion();
+    }
 });
 
 function listener( rsp ) {
     const suggestedListElements = rsp[1].map( item => `<li>${item[0]}</li>`);
     suggestions.innerHTML = '';
     suggestions.innerHTML = suggestedListElements.join('');
+}
+
+function highlightSuggestion() {
+    const suggList = document.querySelectorAll('li');
+    suggList.forEach( element => {
+        element.classList.remove('active');
+    });
+    suggList[sIndex].classList.add('active');
+    input.value = suggList[sIndex].innerText;
+    sIndex += 2;
+    sIndex = ( sIndex/2 == suggList.length/2 ) ? 1 : sIndex;
+    sIndex = ( sIndex >= suggList.length ) ? 0 : sIndex;
 }
 
 function removeSuggestions() {
